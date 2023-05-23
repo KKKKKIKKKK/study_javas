40bec32c92022b7020022d51415aa56995b5763c
Your Name <you@example.com>
2023-05-23 17:35:47 +0900

    스테이징

---
+++ src/Ifs.java
@@ -0,0 +1,18 @@
+public class Ifs {
+    public static void main(String[] args) throws Exception {
+
+        // if (condition) {
+        // block of code to be executed if the condition is true
+        // } else{
+//
+       // }
+       int first = 20;
+       int second= 18;
+       if (first > second) {
+        System.out.print(first) ;
+       }
+
+        System.out.println("Hello, World!");
+        // return 0;
+    }
+}
