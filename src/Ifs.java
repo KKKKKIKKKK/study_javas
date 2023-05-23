f49be4ce58185856f4f0bd47c5a3207f125c9806
Your Name <you@example.com>
2023-05-23 18:35:59 +0900

    복붙

--- src/Ifs.java
+++ src/Ifs.java
@@ -6,11 +6,42 @@ public class Ifs {
         // } else{
 //
        // }
+       //변수선언 숫자int
        int first = 20;
        int second= 18;
        if (first > second) {
         System.out.print(first) ;
        }
+
+     //  {아침인지 점심인지 저녁인지 if 문으로 작성}
+     //12시 이전은 오전 , 18시 이전은 오후 ,24시까지는 밤 
+
+     int time =15;
+
+     if(time<12) {
+        System.out.println("오전입니다");
+     }
+     if(time<18) {
+        System.out.println("오후입니다");
+     }
+
+     if(time<24) {
+        System.out.println("밤입니다");
+     }
+
+
+     if(time<12) {
+        System.out.println(x:"오전입니다");
+     }
+
+     else if(time<18) {
+        System.out.println(x:"오후입니다");
+     }
+
+     if(time<24) {
+        System.out.println(x:"밤입니다");
+     }
+
 
         System.out.println("Hello, World!");
         // return 0;
