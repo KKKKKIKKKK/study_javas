e87b63a65cc7de7e9bdc9fde3b7a72dfad8f9c9e
Your Name <you@example.com>
2023-05-23 16:11:44 +0900

    스테이징

--- src/MakingGamebyOneHand.java
+++ src/MakingGamebyOneHand.java
@@ -1,8 +1,8 @@
 public class MakingGamebyOneHand {
     public static void main(String[] args) {
-        String first = "첫째" ;
-        String second = "둘째" ;
-        String third = "셋째" ;
+        String first = "one" ;
+        String second = "two" ;
+        String third = "three" ;
       
         third = first;
         System.out.println(third);
