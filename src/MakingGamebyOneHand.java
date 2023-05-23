0b8730263cfa52bb61bd62fa51a636a6473aa1ef
Your Name <you@example.com>
2023-05-23 16:08:54 +0900

    변수 선언2

--- src/MakingGamebyOneHand.java
+++ src/MakingGamebyOneHand.java
@@ -1,8 +1,8 @@
 public class MakingGamebyOneHand {
     public static void main(String[] args) {
-        String first = "1" ;
-        String second = "2" ;
-        String third = "" ;
+        String first = "첫째" ;
+        String second = "둘째" ;
+        String third = "셋째" ;
       
         third = first;
         System.out.println(third);
