14e342dac9a7b72a3e9cf46ad6eb6d5f0a7bf643
Your Name <you@example.com>
2023-05-23 16:02:35 +0900

    변수 퀘스트

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,9 @@ public class Main {
     public static void main(String[] args) {
         String name = "minjeong kim" ;
         System.out.println(name);
+        name = "kim";
+        System.out.println();
+        
         //return 0;
     }
     
---
+++ src/MakingGamebyOneHand.java
@@ -0,0 +1,16 @@
+public class MakingGamebyOneHand {
+    public static void main(String[] args) {
+        String first = "1" ;
+        String second = "2" ;
+        String third = "" ;
+      
+        third = first;
+        System.out.println(third);
+        third = second;
+        System.out.println(third);
+        //return 0;
+
+    
+    }
+    
+}
