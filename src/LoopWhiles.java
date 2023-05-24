f303c8c1ca43e25f4841bdca91584b3bf35ee163
Your Name <you@example.com>
2023-05-24 11:10:02 +0900

    LoopWhile

---
+++ src/LoopWhiles.java
@@ -0,0 +1,25 @@
+public class LoopWhiles {
+
+    public static void main(String[] args) {
+        // while (condition) {
+        // code block to be executed
+        // }
+        // ①선생님이 int값을 임의로 배정 first(변수)=5는 값
+        int repeatcount_A = 5;
+        int number_B = 1;
+        int count_C = 1;
+        System.out.println(repeatcount_A + number_B + count_C);
+
+        /// ②"5와 10을 비교할 부호 넣기"
+        // first가 작다는 것을 비교
+        while (count_C <= repeatcount_A) {
+            // first 프린트
+            System.out.println("num_B");
+            count_C = count_C + 1;
+            number_B = number_B + 1;
+
+        }
+
+        // return0;
+    }
+}
---
+++ src/LoopWithCounter.java
@@ -0,0 +1,22 @@
+public class LoopWithCounter {
+    
+    public static void main(String[] args) {
+        //while (condition) {
+        // code block to be executed
+        //  }
+            //①선생님이 int값을 임의로 배정 first(변수)=5는 값
+        int first = 5 ;
+        int second = 10;
+            /// ②"5와 10을 비교할 부호 넣기" 
+            //first가 작다는 것을 비교 
+        while (first  < second) {
+            //first 프린트 
+            System. out.println(first);
+            first = first + 2 ;
+
+        }
+        System. out.println();
+        //return0;
+    }
+}
+
