02e89ba1f6aa9ba11481d6269b8291d41ec965eb
Your Name <you@example.com>
2023-05-24 15:36:33 +0900

    업로드

--- src/LoopWhiles.java
+++ src/LoopWhiles.java
@@ -5,18 +5,18 @@ public class LoopWhiles {
         // code block to be executed
         // }
         // ①선생님이 int값을 임의로 배정 first(변수)=5는 값
-        int repeatcount_A = 5;
-        int number_B = 1;
-        int count_C = 1;
+        int first= 5;
+        int second = 10;
+       
         
 
         /// ②"5와 10을 비교할 부호 넣기"
         // first가 작다는 것을 비교
-        while (count_C <= repeatcount_A) {
+        while (first< second) {
             // first 프린트
-            System.out.println("count_c");
-            count_C = count_C + 1;
-            number_B = number_B + 1;
+            System.out.println(first);
+            first= first + 2;
+           
 
         }
 
---
+++ src/LoopWithMark_Code.java
@@ -0,0 +1,18 @@
+public class LoopWithMark_Code {
+    public static void main(String[] args) {
+        // for (statement 1; statement 2; statement 3) {
+        // code block to be executed
+        // }
+           
+
+        int second = 5;
+        String s = "for ";
+        for (int line= 1; line< second ;  line = line  +1 ) {
+        // code block to be executed
+        System.out.println( s +line);
+        }
+        System.out.println();
+        // return 0;
+    }
+
+}
