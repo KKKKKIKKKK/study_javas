49a2352b5c9551fd389234ddfd2fd9492b0085e3
Your Name <you@example.com>
2023-05-31 11:45:19 +0900

    재업로드

--- src/GitConflicts.java
+++ src/GitConflicts.java
@@ -1,9 +1,15 @@
 public class GitConflicts {
     public static void main(String[] args) {
+        try {
+            
+      
         int second = 1;
         int first = 0;
         second = first + 1;  
-
+        
+         } catch (Exception e) {
+        // TODO: handle exception
+        }
         // return 0;
     }
     
