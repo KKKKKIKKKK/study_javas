a2d1aea52ad083d5cb11e4cfd461560bd2573371
Your Name <you@example.com>
2023-05-31 11:09:21 +0900

    newegithub second

---
+++ src/GitTemplates.java
@@ -0,0 +1,18 @@
+public class GitTemplates {
+    public static void main(String[] args) {
+        try {
+            
+            int first =0 ;  //1. first 는 동일 
+            int second = 1 ;  //2. second 추가. 
+
+            second =second +1 ;
+
+
+
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+
+        return0;
+    }
+}
