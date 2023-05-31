696128798486d8f719759ad5e623522691b95b93
Your Name <you@example.com>
2023-05-31 10:51:33 +0900

    add gitignore

--- src/MethodsSwitchs.java
+++ src/MethodsSwitchs.java
@@ -1,34 +1,50 @@
 public class MethodsSwitchs {
-  // <access_modifier> <return_type> <method_name>( list_of_parameters)
+    // <access_modifier> <return_type> <method_name>( list_of_parameters)
     // {
     // //body
     // }
 
-    
-    public String today(int day ) {
-        String str="";
+    public String today(int day) {
+        String str = "";
         try {
-           
+            switch (day) {
+                case 1:
+                    str = "Monday";
+                    break;
+
+                case 2:
+                    str = "Tudesday";
+                    break;
+
+                case 3:
+                    str = "Wednesday";
+                    break;
+
+                case 4:
+                    str = "Thursday ";
+                    break;
+
+            }
+
         } catch (Exception e) {
             // TODO: handle exception
         }
-      
-    
-        return str; 
+
+        return str;
     }
+
     public static void main(String[] args) {
         try {
-            int day = 4;  // 1~4 가능 
-            String str = ""; //string 에 변수 넣음 
+            int day = 4; // 1~4 가능
+            String str = ""; // string 에 변수 넣음
 
-        
             MethodsSwitchs methodsSwitchs = new MethodsSwitchs();
-           String returnResult =  methodsSwitchs.today(day);
-            System.out.println( returnResult);
-                
-            } catch (Exception e) {
-                // TODO: handle exception}
-            }
-            //return 0;
+            String returnResult = methodsSwitchs.today(day);
+            System.out.println(returnResult);
+
+        } catch (Exception e) {
+            // TODO: handle exception}
+        }
+        // return 0;
     }
 }
---
+++ src/methodsSwitchs/today.java
@@ -0,0 +1,5 @@
+package methodsSwitchs;
+
+public class today {
+
+}
