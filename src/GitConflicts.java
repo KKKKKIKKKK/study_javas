9569a32a9d39c5f450df147b9887adf98a05c50d
Your Name <you@example.com>
2023-05-31 11:37:56 +0900

    공유 작성 파일

---
+++ src/GitConflicts.java
@@ -0,0 +1,10 @@
+public class GitConflicts {
+    public static void main(String[] args) {
+        int second = 1;
+        int first = 0;
+        second = first + 1;  
+
+        // return 0;
+    }
+    
+}
