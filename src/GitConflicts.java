1004d1d9c1f33ea8bb329279f22c25abcb1da52c
Your Name <you@example.com>
2023-05-31 11:46:46 +0900

    푸쉬

--- src/GitConflicts.java
+++ src/GitConflicts.java
@@ -1,10 +1,17 @@
 public class GitConflicts {
     public static void main(String[] args) {
+        try {
+            
+      
         int first = 2;
         first = first + 1;  
         int second = 1;
         int first_1 = 0;
         second = first + 1;  
+
+    } catch (Exception e) {
+        // TODO: handle exception
+    }
 
         // return 0;
     }
