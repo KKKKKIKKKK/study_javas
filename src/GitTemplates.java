c3397d5b3616516143177cbc4552f4646954174b
Your Name <you@example.com>
2023-05-31 11:08:15 +0900

    new gits

---
+++ src/GitTemplates.java
@@ -0,0 +1,16 @@
+public class GitTemplates {
+    public static void main(String[] args) {
+        try {
+            
+            int first = 0;  //int 변수는 0
+            first = first+ 1 ; 
+
+
+
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+
+        return0;
+    }
+}
