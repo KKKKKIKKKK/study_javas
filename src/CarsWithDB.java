be7fa222083aca3fad3d106bc8544a62a018f0c0
Your Name <you@example.com>
2023-06-12 18:24:51 +0900

    ------ 작동 key ------

--- src/CarsWithDB.java
+++ src/CarsWithDB.java
@@ -1,19 +1,22 @@
 import java.util.Scanner;
+// //------ 작동 key ------
+// (E)xit : 종료                            
+// (O)ption : 옵션 추가 시작
+// (S)tatistic : 옵션 갯수 통계 
+import java.util.Scanner;
 
 public class CarsWithDB {
-
     public static void main(String[] args) {
         try {
             Scanner scanner = new Scanner(System.in);
-          
-            String workkey = scanner.nextLine();
-            while(!workkey.equals("E")){
-                System.out.println("선택입력: : ");
-                workkey = scanner.nextLine() ; 
+            String workKey = "P";
+            while(!workKey.equals("E")){
+                System.out.print("선택입력 : ");
+                workKey = scanner.nextLine();
             }
-           
         } catch (Exception e) {
-            System.out.println( e.getMessage());
+            // TODO: handle exception
+            System.out.println(e.getMessage());
         }
         System.out.println();
     }
