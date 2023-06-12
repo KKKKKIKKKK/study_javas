c645e2eabfc81fc234e7c3dca554ec7a50490045
Your Name <you@example.com>
2023-06-12 13:05:55 +0900

    T_재업로드

--- src/T_ConnectDBs0612.java
+++ src/T_ConnectDBs0612.java
@@ -3,7 +3,6 @@ import java.sql.*;
 public class T_ConnectDBs0612 {
     public static void main(String[] args) {
        
-       
         try {
             // - MySQL workbench 실행 : JDBCs
             // - User/password와 접속 IP:port 접속
@@ -12,7 +11,7 @@ public class T_ConnectDBs0612 {
             String password = "!yojulab*";
 
             Connection connection = DriverManager.getConnection(url, user, password);
-                System.out.println("DB연결 성공\n");
+            System.out.println("DB연결 성공\n");
 
             // - query Edit
             Statement statement = connection.createStatement();
@@ -21,33 +20,36 @@ public class T_ConnectDBs0612 {
             while (resultSet.next()) {
                 System.out.println(resultSet.getString("COMPANY_ID") + resultSet.getString("COMPANY"));
             }
-            //SELECT COUNT(*) AS CNT FROM factorys;
-            query = "SELECT COUNT(*) AS CNT FROM factorys ";
-
+            
+            // SELECT COUNT(*) AS CNT FROM factorys;
+            query = "SELECT COUNT(*) AS CNT FROM factorys";
             resultSet = statement.executeQuery(query);
-            int totalCount = 0 ; 
-
-             while (resultSet.next()) {
-                System.out.println( resultSet.getInt("CNT"));
+            int totalCount = 0;
+            while (resultSet.next()) {
+                System.out.println(resultSet.getInt("CNT"));
                 totalCount = resultSet.getInt("CNT");
             }
-            // INSERT INTO factorys 
-            // (COMPANY_ID , COMPANY)
-            // VALUE
-            // ('CAR-01' , 'AUDI'); 
-            String companyId = "CAR- 01";               //companyId 변수설정 
-            String company = "AUDI";                    //company 변수설정 
-            query = "INSERT INTO factorys " +           // 스페이스 찍어줌. 
-                    "(COMPANY_ID , COMPANY) " +         //COMPANY_ID , COMPANY 를 위에 변수로 설정 
-                    " VALUE " + 
-                    "('"+companyId+"', '"+company+"') "; //쿼리안에 ' " +변수로 넣어준 상태에서 진행. 
+
+            /*
+            INSERT INTO factorys
+            (COMPANY_ID, COMPANY) 
+            VALUE 
+            ('CAR-01', 'AUDI');
+            */
+            String companyId = "CAR-01";
+            String company = "AUDI";
+            query = "INSERT INTO factorys " +
+                    "(COMPANY_ID, COMPANY) " +
+                    "VALUES " +
+                    "('"+companyId+"', '"+company+"') ";
            
-           
-            int count =  statement.executeUpdate(query);
+            int count = statement.executeUpdate(query);
+            System.out.println("추가된 레코드 수: " + count);
 
             // UPDATE factorys
-           
-      companyId = "CAR-01";
+            // SET COMPANY = '페라리'
+            // WHERE COMPANY_ID = 'CAR-01';
+            companyId = "CAR-01";
             company = "페라리";
             query = "UPDATE factorys " +
                     "SET COMPANY = '"+company+"' " +
@@ -56,7 +58,7 @@ public class T_ConnectDBs0612 {
             System.out.println("업데이트된 레코드 수: " + count);
 
             // DELETE FROM factorys
-          
+            // WHERE COMPANY_ID = 'CAR-01';
             companyId = "CAR-01";
             query = "DELETE FROM factorys " +
                     "WHERE COMPANY_ID = '"+companyId+"' ";
