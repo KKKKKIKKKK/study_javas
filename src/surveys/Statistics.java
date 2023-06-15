5ba274d7580c1cee28cd8a42a61f343c71ec7149
Your Name <you@example.com>
2023-06-15 17:18:30 +0900

    push

---
+++ src/surveys/Statistics.java
@@ -0,0 +1,36 @@
+package surveys;
+
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class Statistics {
+    public int getRespondents(Statement statement){
+
+        try {
+             System.out.println("--- 통계 ---");
+            // -- 총 설문자  : 3명 
+            String queryA = "SELECT  count(*) CNT\r\n" + //
+                    "FROM(\r\n" + //
+                    "\tSELECT RESPONDENTS_ID , count(*) CNT\r\n" + //
+                    "\tFROM statistics\r\n" + //
+                    "\tgroup BY RESPONDENTS_ID \r\n" + //
+                    ")AS  T_STATISTICS";
+            ResultSet resultSet = statement.executeQuery(queryA);
+            while (resultSet.next()) {
+                System.out.println("-- 총 설문자 : " + resultSet.getString("CNT") );
+            }
+            
+
+
+
+        } catch (Exception e) {
+
+            System.out.println();
+            // TODO: handle exception
+        }
+
+
+         return 0 ; 
+    }
+    
+}
