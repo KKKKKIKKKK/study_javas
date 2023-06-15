8a8a46be3685702b9db640c22bd9c6e9e5fc3ee5
jeongjiwooon <jjwanm@naver.com>
2023-06-15 16:57:16 +0900

    B push

--- src/SurveysApp.java
+++ src/SurveysApp.java
@@ -9,19 +9,26 @@ public class SurveysApp
     {
         try
         {
-            String url = "jdbc:mysql://127.0.0.1:3306/db_cars";
+            String url = "jdbc:mysql://127.0.0.1:3306/db_survey";
             String user = "root";
             String password = "12345678";
 
             Connection connection = DriverManager.getConnection(url, user, password);
             System.out.println("DB연결 성공\n");
 
+            System.out.println("--- 통계 ---");
+            // -- 총 설문자: 3명
             Statement statement = connection.createStatement();
-            String query = "SELECT * FROM factorys";
-            ResultSet resultSet = statement.executeQuery(query);
+
+            String queryB = "select count(*) CNT\r\n" + //
+                    "from (\r\n" + //
+                    "select respondents_id, count(*) as CNT\r\n" + //
+                    "from statistics\r\n" + //
+                    "group by respondents_id) as T_static;";
+            ResultSet resultSet = statement.executeQuery(queryB);
             while (resultSet.next())
             {
-                System.out.println(resultSet.getString("COMPANY_ID") + resultSet.getString("COMPANY"));
+                System.out.println("-- 총 설문자: " + resultSet.getString("CNT"));
             }
         }
         catch (Exception e)
