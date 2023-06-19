354a578c787447933e900309db64704b1f3ef385
Your Name <you@example.com>
2023-06-19 17:29:48 +0900

    commit

--- src/surveys/Inheritances.java
+++ src/surveys/Inheritances.java
@@ -3,49 +3,43 @@ package surveys;
 import java.util.HashMap;
 import java.util.Map;
 
-// import Inheritances.ObjectInheritances;
-// import vehicles.Bicycle;
-import vehicles.Bike;
+import Inheritances.ObjectInheritances;
+import vehicles.Bicycle;
 import vehicles.Car;
 // import vehicles.MountainBike;
-// import vehicles.RoadCycle;
-import vehicles.Vehicle;
+import vehicles.RoadCycle;
 
-public class Inheritances
-{
-    public static void main(String[] args)
-    {
-        try
-        {
+public class Inheritances {
+    public static void main(String[] args) {
+        try {
             // Object 확인
             // ObjectInheritances objectInheritances = new ObjectInheritances();
 
             // 상속할 class 생성
             // Bicycle bicycle = new Bicycle(5500, "blue");
-            // System.out.println(bicycle.toString());
+            // System.out.println(★ bicycle.toString());
 
             // mountain 상속해 활용
             // MountainBike mountainBike = new MountainBike();
             // MountainBike mountainBike = new MountainBike(6200, "yellow", 5);
-            // System.out.println();
+            // System.out.println(★ mountainBike.toString());
 
-            // RoadCycle roadCycle = new  RoadCycle (10000, "blue", "정지운 자전거 ");
-            //  System.out.println(roadCycle);
+            // RoadCycle roadCycle = new RoadCycle (10000, "blue", "정지운 자전거 ");
+            // System.out.println(roadCycle .toString());
 
             // 구현하고 호출하기
-            Car car = new Car();
+            car car = new car();
+
             int speed = car.speedUp(1);
             speed = car.speedDown(2);
 
             Vehicle bike = new Bike();
-            Vehicle car_second = new Car();
+            Vehicle car_second = new car(); 
+             
             System.out.println();
 
-            Map<String, String> hashMap = new HashMap<>();
-
-        }
-        catch (Exception e)
-        {
+            Map<String, String> hashMap = new HashMap<>(); 
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
     }
