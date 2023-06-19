28cdc273d6f883edf71d2857fd36ac188021826b
jeongjiwooon <jjwanm@naver.com>
2023-06-19 13:18:14 +0900

    B commit

---
+++ src/Inheritances/ObjectInheritances.java
@@ -0,0 +1,17 @@
+package Inheritances;
+
+public class ObjectInheritances extends Object
+{
+    public int FirstMethods(String first)
+    {
+        try
+        {
+            System.out.println("FirstMethods");
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+        }
+        return 0;
+    }
+}
