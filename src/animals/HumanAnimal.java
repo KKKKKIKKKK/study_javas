9680db6c742ead8a50e716d0fcfa7f95384e25e9
jeongjiwooon <jjwanm@naver.com>
2023-06-20 09:20:34 +0900

    B commit

--- src/animals/HumanAnimal.java
+++ src/animals/HumanAnimal.java
@@ -7,8 +7,8 @@ public class HumanAnimal extends HumanAnimalExtend
         String str = "Legs: " + this.setLegs + " " + "Arms: " + this.setArms + " " + "Speech: " + this.setEnableSpeech;
         return str;
     }
-        public String getSpeed()
-        {
+    public String getSpeed()
+    {
         HumanAnimalExtend humanAnimalExtend = new HumanAnimalExtend();
         this.speed = humanAnimalExtend.speed(1);
         String spd = "Speed: " + this.speed + "km";
